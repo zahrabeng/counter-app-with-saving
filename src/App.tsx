ec0ad2184b174ccae071f29e05dc980772babcc8
Richard Ng <richardcrng@gmail.com>
2021-06-27 18:31:19 +0100

    Add NumberPicker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { greet } from "./utils/greet";
+import NumberPicker from "./components/NumberPicker";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <NumberPicker />;
 }
 
 export default App;
---
+++ src/components/NumberPicker.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function NumberPicker(): JSX.Element {
+  const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] = useState(0);
+  const [favouriteValueFromCurrentRender, queueRerenderWithNewFavouriteValue] = useState(0);
+
+  const handleAddOneToCounter = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
+  }
+
+  const handleSubtractOneFromCounter = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+  };
+
+  const handleStoreCurrentCount = () => {
+    queueRerenderWithNewFavouriteValue(counterValueFromCurrentRender);
+  }
+
+  return (
+    <>
+      <h1>Number picker</h1>
+      <p>Your stored number: {favouriteValueFromCurrentRender}</p>
+      <p>Counter: {counterValueFromCurrentRender}</p>
+      <button onClick={handleSubtractOneFromCounter}>-1</button>
+      <button onClick={handleAddOneToCounter}>+1</button>
+      <hr />
+      <button onClick={handleStoreCurrentCount}>Store current count</button>
+    </>
+  )
+}
