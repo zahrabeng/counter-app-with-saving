076fe193b64aef0a6b9a1d6f12b436992b51cbd2
Zahra <bghzahra@outlook.com>
2022-03-08 20:08:51 +0000

    added emoji picker working stored values for each emoji

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,2 +1,41 @@
 import { useState } from "react";
 
+export default function EmojiPicker (): JSX.Element{
+    
+    const [emojiValueFromCurrentRender, queRenderWithEmojiValue] = useState ("")
+    const [storedValueFromCurrentRender, queRenderWithStoredValue] = useState ("")
+
+    const HandleRenderClockEmoji = () => {
+        queRenderWithStoredValue(emojiValueFromCurrentRender)
+        queRenderWithEmojiValue("⏰") 
+    }
+
+    const HandleRenderPanEmoji = () => {
+        queRenderWithStoredValue(emojiValueFromCurrentRender)
+
+        queRenderWithEmojiValue("🍳")
+    }
+   
+    const HandleRenderPlateEmoji = () => {
+        queRenderWithStoredValue(emojiValueFromCurrentRender)
+        queRenderWithEmojiValue("🍽️")
+    }
+
+    const HandleRenderSmileEmoji = () => {
+        queRenderWithStoredValue(emojiValueFromCurrentRender)
+        queRenderWithEmojiValue("😋")
+    }
+    
+return (
+     <div>
+        <h1>Emoji Picker</h1>
+        <p>Your old emoji: {storedValueFromCurrentRender} </p>
+        <p>Your current emoji: {emojiValueFromCurrentRender}</p>
+
+        <button onClick={HandleRenderClockEmoji}>⏰</button>
+        <button onClick={HandleRenderPanEmoji}>🍳</button>
+        <button onClick={HandleRenderPlateEmoji}>🍽️</button>
+        <button onClick={HandleRenderSmileEmoji}>😋</button>
+    </div>
+    )
+}
